3519941503161425fe4e6132e2d261315e9864bd
louisgarrett <louisgarrett@comcast.net>
2016-08-18 12:26:54 -0400

    Day10 - Fix pagination bug

--- RAS/day10/assets/js/index.js
+++ RAS/day10/assets/js/index.js
@@ -1,18 +1,18 @@
 var messagesContainer = $('#messagesContainer');
-var type="";
+var type = "";
 var perPage = 7;
 var msgId;
 var apiUpdate;
 
 function updateCommentsShown(pageNum) {
-  if($("#creatorName").html().indexOf('<i class="glyphicon glyphicon-user" aria-hidden="true">') != -1 || $("#creatorName").html().indexOf('<i class="glyphicon glyphicon-user" aria-hidden="true"></i> All Authors') != -1) {
-    if($("#isImportant").is(':checked')){
+  if ($("#creatorName").html().indexOf('<i class="glyphicon glyphicon-user" aria-hidden="true">') != -1 || $("#creatorName").html().indexOf('<i class="glyphicon glyphicon-user" aria-hidden="true"></i> All Authors') != -1) {
+    if ($("#isImportant").is(':checked')) {
       loadComments("allImportant", pageNum);
     } else {
       loadComments("all", pageNum);
     }
   } else {
-    if($("#isImportant").is(':checked')){
+    if ($("#isImportant").is(':checked')) {
       loadComments("byCreatorImportant", pageNum);
     } else {
       loadComments("byCreator", pageNum);
@@ -20,40 +20,51 @@ function updateCommentsShown(pageNum) {
   }
 }
 
-function updateDropdown(selectedName){$("#creatorName").html(selectedName + '&nbsp;<span class="caret"></span>');}
+function updateDropdown(selectedName) {
+  $("#creatorName").html(selectedName + '&nbsp;<span class="caret"></span>');
+}
 
-function filterComments(jsonString, filterBy){
+function filterComments(jsonString, filterBy) {
   var byName, tmpJson, tmp;
   var destringify = JSON.parse(jsonString);
   //byName ="Lou";
-  switch(filterBy){
+  switch (filterBy) {
     case "allImportant":
       notyAlert('info', 'Showing All Important Messages');
-      response = $(destringify).filter(function(i, n){
-        return   n.isImportant !== false;
+      response = $(destringify).filter(function(i, n) {
+        return n.isImportant !== false;
       });
       break;
     case "byCreator":
-      byName = $("#creatorName").html().slice(0,-33);
+      byName = $("#creatorName").html().slice(0, -33);
       notyAlert('info', 'Showing Messages by ' + byName + '.');
-      response = $(destringify).filter(function(i, n){
-        return   n.createdBy === byName;
+      response = $(destringify).filter(function(i, n) {
+        return n.createdBy === byName;
       });
       break;
     case "byCreatorImportant":
-      byName = $("#creatorName").html().slice(0,-33);
+      byName = $("#creatorName").html().slice(0, -33);
       notyAlert('info', 'Showing All Important Messages by ' + byName + '.');
-      response = $(destringify).filter(function(i, n){
+      //v1 following above format
+      /*response = $(destringify).filter(function(i, n){
         return   n.createdBy === byName && n.isImportant !== false;
-      });
+      });*/
+
+      //v2 recommended by w3schools
+      response = [];
+      for (var i = 0; i < destringify.length; i++) {
+        if (destringify[i].createdBy === byName && destringify[i].isImportant) {
+          response.push(destringify[i]);
+        }
+      }
       console.log(response);
       break;
     default:
       notyAlert('info', 'Showing All Messages.');
       response = destringify;
-      
+
   }
-  
+
   tmp = '[';
   $.each(response, function(i, v) {
     tmp += '{';
@@ -67,20 +78,20 @@ function filterComments(jsonString, filterBy){
   });
   tmp = tmp.substring(0, tmp.length - 1);
   tmp += ']';
-  
-  return(tmp);
+
+  return (tmp);
 }
 
 function loadComments(action, pageNum) {
   var callType, byName, z;
-  if(apiUpdate){
+  if (apiUpdate) {
     console.log("Using API Return values.");
     z = filterComments(apiUpdate, action);
     showMessages(JSON.parse(z), pageNum);
   } else {
     console.log("New call to server.");
-    $.get(apiEndpointBase + '/comments', function(response, status){
-      if(status === 'success'){
+    $.get(apiEndpointBase + '/comments', function(response, status) {
+      if (status === 'success') {
         apiUpdate = JSON.stringify(response);
         z = filterComments(apiUpdate, action);
         showMessages(JSON.parse(z), pageNum);
@@ -94,26 +105,26 @@ function addMessage() {
   toggleSpinner(true);
   var myData = {
     comment: {
-      createdBy:$("#createdBy")[0].value, 
-      commentText:$("#commentText")[0].value, 
-      isImportant:$("#isImportant")[0].checked
+      createdBy: $("#createdBy")[0].value,
+      commentText: $("#commentText")[0].value,
+      isImportant: $("#isImportant")[0].checked
     }
   };
   myData = JSON.stringify(myData);
-                
+
   var jqxhr = $.ajax({
-    url: apiEndpointBase + "/comments", 
+    url: apiEndpointBase + "/comments",
     type: "POST",
     data: myData,
-    contentType:"application/json; charset=utf-8",
-    dataType:"json",
-    success: function(result){
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(result) {
       apiUpdate = JSON.stringify(result);
       updateCommentsShown(1);
       notyAlert('success', 'Your new message has been added to the board.');
       toggleSpinner(false);
     },
-    error: function(){
+    error: function() {
       notyAlert('error', 'Failed to add message, please try again.');
       toggleSpinner(false);
     }
@@ -122,8 +133,8 @@ function addMessage() {
 
 function getMessage(messageId) {
   msgId = messageId;
-  $.get(apiEndpointBase + "/comments/" + msgId, function(response, status){
-    if(status === "success"){
+  $.get(apiEndpointBase + "/comments/" + msgId, function(response, status) {
+    if (status === "success") {
       $("textarea#editCommentText.form-control").val(response.commentText);
       $("input#editIsImportant").prop("checked", response.isImportant);
     }
@@ -134,26 +145,26 @@ function editMessage() {
   toggleSpinner(true);
   var myData = {
     comment: {
-      commentText:$("#editCommentText").val(), 
-      isImportant:$("#editIsImportant").prop("checked")
+      commentText: $("#editCommentText").val(),
+      isImportant: $("#editIsImportant").prop("checked")
     }
   };
   myData = JSON.stringify(myData);
-  
+
   var jqxhr = $.ajax({
-    url: apiEndpointBase + "/comments/" + msgId, 
+    url: apiEndpointBase + "/comments/" + msgId,
     type: "PUT",
     data: myData,
-    contentType:"application/json; charset=utf-8",
-    dataType:"json",
-    success: function(result){
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(result) {
       apiUpdate = JSON.stringify(result);
       updateCommentsShown(1);
       notyAlert('success', 'Messsage has been updated.');
       toggleSpinner(false);
     },
-    error: function(){
-       notyAlert('error', 'Failed to update message, please try again.');
+    error: function() {
+      notyAlert('error', 'Failed to update message, please try again.');
       toggleSpinner(false);
     }
   });
@@ -166,13 +177,13 @@ function deleteMessage(messageId) {
     $.ajax({
       url: apiEndpointBase + '/comments/' + messageId,
       type: 'DELETE',
-      success: function(result){
+      success: function(result) {
         apiUpdate = JSON.stringify(result);
         updateCommentsShown(1);
         notyAlert('success', 'Message has been deleted.');
         toggleSpinner(false);
       },
-      error: function(){
+      error: function() {
         notyAlert('error', 'Failed to delete message, please try again.');
         toggleSpinner(false);
       }
@@ -180,12 +191,14 @@ function deleteMessage(messageId) {
   }
 }
 
-function toggleSpinner(isVisible) {document.getElementById("loading").classList[isVisible ? 'add' : 'remove']('visible');}
+function toggleSpinner(isVisible) {
+  document.getElementById("loading").classList[isVisible ? 'add' : 'remove']('visible');
+}
 
 function showMessages(messages, pageNum) {
-  var i, message, n, x, y, h1, h2, h3,c1, c2, c3, t1, t2, t3, messageData, msgDate;
-  
-  if(!pageNum){
+  var i, message, n, x, y, h1, h2, h3, c1, c2, c3, t1, t2, t3, messageData, msgDate;
+
+  if (!pageNum) {
     pageNum = 0;
   }
   if (typeof messages === 'string') {
@@ -196,7 +209,7 @@ function showMessages(messages, pageNum) {
     if (a.updatedAt > b.updatedAt) {
       return -1;
     }
-    
+
     if (a.updatedAt < b.updatedAt) {
       return 1;
     }
@@ -205,6 +218,106 @@ function showMessages(messages, pageNum) {
   });
 
   messagesContainer.html("");
+
+  switch (type) {
+    case "table":
+      x = $('<table id="messageTable" class="col-lg-12"></table>)');
+      $("#messagesContainer").append(x);
+
+      y = $('<tr id="headRow"></tr>');
+      $('#messageTable').append(y);
+
+      h1 = $('<th class="col-lg-6">Message</th>');
+      h2 = $('<th class="col-lg-4">Author</th>');
+      h3 = $('<th class="col-lg-2">Actions</th>');
+      $('#headRow').append(h1, h2, h3);
+
+      for (i = ((pageNum * perPage) - perPage); i < (pageNum * perPage); i++) {
+        message = messages[i];
+
+        if (message.createdAt === message.updatedAt) {
+          msgDate = 'Created ' + moment(message.createdAt).fromNow();
+        } else {
+          msgDate = 'Last updated ' + moment(message.updatedAt).fromNow();
+        }
+
+        y = $('<tr id="r' + i + '"></tr>');
+        $('#messageTable').append(y);
+
+        c1 = $('<td class="comment col-lg-6"></td>').html((message.isImportant ? '<span class="label label-danger">IMPORTANT</span>&ensp;' : '') + message.commentText);
+        c2 = $('<td class="author col-lg-4"></td>').html(message.createdBy + '<br />' + msgDate);
+        c3 = $('<td class="actions col-lg-2"></td>').html('<button class="btn btn-primary" id="editMessage' + message.id + '"><i class="glyphicon glyphicon-pencil"></i></button>' + '<button class="btn btn-danger" id="deleteMessage' + message.id + '"><i class="glyphicon glyphicon-trash"></i></button>');
+        $("#r" + i).append(c1, c2, c3);
+
+        var s = message.id;
+        $("#editMessage" + message.id).click(
+          (function() {
+            var t = s;
+            return function() {
+              createModalDialog('./editMsg.html', 'edit');
+              getMessage(t);
+            }
+          })(s)
+        );
+        $("#deleteMessage" + message.id).click(
+          (function() {
+            var t = s;
+            return function() {
+              deleteMessage(t);
+            }
+          })(s)
+        );
+      }
+      break;
+
+    default:
+      for (i = ((pageNum * perPage) - perPage); i < (pageNum * perPage); i++) {
+        message = messages[i];
+        var messageDiv = $('<div class="message"></div>'); //document.createElement("div");
+        var messageTextDiv = $('<div></div>'); //document.createElement("div");
+        var messageDateDiv = $('<p class="date"></p>'); //document.createElement("p");
+
+        // message header
+        var messageHtml = '<p>' + message.createdBy + (message.isImportant ? '&#160;<span class="label label-danger">IMPORTANT</span>' : '') +
+          '<button class="btn btn-danger pull-right" id="deleteMessage' + message.id + '"><i class="glyphicon glyphicon-trash"></i>' +
+          '</button><button class="btn btn-primary pull-right" id="editMessage' + message.id + '"><i class="glyphicon glyphicon-pencil"></i></button></p>';
+
+        // message text
+        messageTextDiv.html(message.commentText);
+
+        // message date
+        if (message.createdAt === message.updatedAt) {
+          messageDateDiv.html('Created ' + moment(message.createdAt).fromNow());
+        } else {
+          messageDateDiv.html('Last updated ' + moment(message.updatedAt).fromNow());
+        }
+
+        // update message div
+        $(messageDiv).html(messageHtml);
+        $(messageDiv).append(messageTextDiv);
+        $(messageDiv).append(messageDateDiv);
+
+        $("#messagesContainer").append(messageDiv);
+        var u = message.id;
+        $("#editMessage" + message.id).click(
+          (function() {
+            var v = u;
+            return function() {
+              createModalDialog('./editMsg.html', 'edit');
+              getMessage(v);
+            }
+          })(u)
+        );
+        $("#deleteMessage" + message.id).click(
+          (function() {
+            var v = u;
+            return function() {
+              deleteMessage(v);
+            }
+          })(u)
+        );
+      }
+  }
   
   $('#page-selection').bootpag({
     total: messages.length / perPage,
@@ -225,135 +338,34 @@ function showMessages(messages, pageNum) {
   }).on("page", function(event, num){
     updateCommentsShown(num);
   });
-  
-  switch (type) {
-    case "table":
-      x = $('<table id="messageTable" class="col-lg-12"></table>)');
-      $("#messagesContainer").append(x);
-      
-      y = $('<tr id="headRow"></tr>');
-      $('#messageTable').append(y);
-      
-      h1 = $('<th class="col-lg-6">Message</th>');
-      h2 = $('<th class="col-lg-4">Author</th>');
-      h3 = $('<th class="col-lg-2">Actions</th>');
-      $('#headRow').append(h1, h2, h3);
-      
-      for(i=((pageNum * perPage)-perPage); i < (pageNum * perPage); i++) {
-        message = messages[i];
-        
-        if (message.createdAt === message.updatedAt) {
-          msgDate = 'Created ' + moment(message.createdAt).fromNow();
-        } else {
-          msgDate = 'Last updated ' + moment(message.updatedAt).fromNow();
-        }
-      
-        y = $('<tr id="r'+ i +'"></tr>');
-        $('#messageTable').append(y);
-        
-        c1 = $('<td class="comment col-lg-6"></td>').html((message.isImportant ? '<span class="label label-danger">IMPORTANT</span>&ensp;' : '') + message.commentText);
-        c2 = $('<td class="author col-lg-4"></td>').html(message.createdBy + '<br />' + msgDate);
-        c3 = $('<td class="actions col-lg-2"></td>').html('<button class="btn btn-primary" id="editMessage'+ message.id +'"><i class="glyphicon glyphicon-pencil"></i></button>' + '<button class="btn btn-danger" id="deleteMessage' + message.id + '"><i class="glyphicon glyphicon-trash"></i></button>');
-        $("#r"+i).append(c1, c2, c3);
-        
-        var s = message.id;
-        $("#editMessage"+message.id).click(
-          (function() {
-            var t = s;
-            return function(){
-              createModalDialog('./editMsg.html', 'edit');
-              getMessage(t);
-            }
-          })(s)
-        );
-        $("#deleteMessage"+message.id).click(
-          (function(){
-            var t = s;
-            return function (){
-              deleteMessage(t);
-            }
-          })(s)
-        );
-      }
-      break;
-        
-    default:
-      for(i=((pageNum * perPage)-perPage); i < (pageNum * perPage); i++) {
-        message = messages[i];
-        var messageDiv = $('<div class="message"></div>'); //document.createElement("div");
-        var messageTextDiv = $('<div></div>'); //document.createElement("div");
-        var messageDateDiv = $('<p class="date"></p>'); //document.createElement("p");
-
-        // message header
-        var messageHtml = '<p>' + message.createdBy + (message.isImportant ? '&#160;<span class="label label-danger">IMPORTANT</span>' : '') + 
-            '<button class="btn btn-danger pull-right" id="deleteMessage' + message.id + '"><i class="glyphicon glyphicon-trash"></i>' +
-            '</button><button class="btn btn-primary pull-right" id="editMessage' + message.id + '"><i class="glyphicon glyphicon-pencil"></i></button></p>';
-        
-        // message text
-        messageTextDiv.html(message.commentText);
-
-        // message date
-        if (message.createdAt === message.updatedAt) {
-          messageDateDiv.html('Created ' + moment(message.createdAt).fromNow());
-        } else {
-          messageDateDiv.html('Last updated ' + moment(message.updatedAt).fromNow());
-        }
-
-        // update message div
-        $(messageDiv).html(messageHtml);
-        $(messageDiv).append(messageTextDiv);
-        $(messageDiv).append(messageDateDiv);
-
-        $("#messagesContainer").append(messageDiv);
-        var u = message.id;
-        $("#editMessage"+message.id).click(
-          (function() {
-            var v = u;
-            return function(){
-              createModalDialog('./editMsg.html', 'edit');
-              getMessage(v);
-            }
-          })(u)
-        );
-        $("#deleteMessage"+message.id).click(
-          (function(){
-            var v = u;
-            return function (){
-              deleteMessage(v);
-            }
-          })(u)
-        );
-      }
-    }
-  console.log(" ");
 }
 
-function createModalDialog(url, type){
+function createModalDialog(url, type) {
   $('body').css("overflow", "hidden");
-  $("#dialog").load(url, function(){
+  $("#dialog").load(url, function() {
     $("#dialog").dialog({
       autoOpen: true,
       width: 800,
       modal: true,
       buttons: {
         Submit: function() {
-          switch (type){
+          switch (type) {
             case 'edit':
               editMessage();
               break;
             default:
               addMessage();
           }
-          $( this ).dialog( "close" );
+          $(this).dialog("close");
           $('body').css("overflow", "visible");
         },
-        Cancel: function(){
-          $( this ).dialog( "close" );
+        Cancel: function() {
+          $(this).dialog("close");
           $('body').css("overflow", "visible");
         }
       }
     });
-    switch (type){
+    switch (type) {
       case 'edit':
         $('#dialog').dialog('option', 'title', 'Edit message');
         break;
@@ -369,19 +381,19 @@ $(function() {
   var host = new RegExp('(^.*)(?=\#)', 'i'); // Regex for grabbing character before the # in a string
   var string = action.exec(window.location.href); // Using above RegEx to strip out the navigation
   var hostname = host.exec(window.location.href); // Using the above RegEx to strip out the hostname
-  
+
   toggleSpinner(true);
-  
+
   $('#setView').bootstrapToggle('off');
   $('#isImportant').bootstrapToggle('off');
-  
-  $("#addMessage").click(function(){
+
+  $("#addMessage").click(function() {
     createModalDialog('./addMsg.html', 'add');
   });
 
   if (string) { // If there is a query string to extract
     startPage = string[0].substring(1);
-    if(startPage < 1){
+    if (startPage < 1) {
       startPage = 1;
     }
   } else {
